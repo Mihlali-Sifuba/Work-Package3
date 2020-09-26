ce436525c174c1b3f0ed34117363489bd3f5196d
Mihlali <71899231+Mihlali-Sifuba@users.noreply.github.com>
2020-09-26 23:10:54 +0200

    Update Tutorial3.py

--- Tutorial3.py
+++ Tutorial3.py
@@ -5,9 +5,14 @@ GPIO.setmode(GPIO.BOARD) #Set up board GPIO numbering mode
 GPIO.setup(11, GPIO.OUT, initial=1) #Set GPIO pin 11 as output and set its initial value to High
 
 #Toggle the LEDs
-for i in range(0,10):
-    GPIO.output(11, True)
-    time.sleep(10)
-    GPIO.output(11, False)
-    time.sleep(10)
-GPIO.cleanup()
+def main():
+    for i in range(0,10):
+        GPIO.output(11, True)
+        time.sleep(2)
+        GPIO.output(11, False)
+        time.sleep(2)
+    GPIO.cleanup()
+    
+    
+ if __name__ == '__main__':
+    main()
