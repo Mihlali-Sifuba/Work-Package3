bf9c601ffce0d24c61db5dc6c9828f0775fa7eb6
Mihlali <71899231+Mihlali-Sifuba@users.noreply.github.com>
2020-09-26 22:31:58 +0200

    Update Tutorial3.py

--- Tutorial3.py
+++ Tutorial3.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO #Python library used on the Raspberry Pi
 import time
 
 GPIO.setmode(GPIO.BOARD) #Set up board GPIO numbering mode
-GPIO.output(7, GPIO.OUT) #Set GPIO pin 7 as output
+GPIO.output(11, GPIO.OUT) #Set GPIO pin 11 as output
 
 #Toggle the LEDs
 for i in range(0,10):
-    GPIO.output(7, True)
+    GPIO.output(11, True)
     time.sleep(0.5)
-    GPIO.output(7, False)
+    GPIO.output(11, False)
 GPIO.cleanup()
